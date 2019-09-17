5da52d189a0c3096c96f42281c70e3fd4ae0997f
amirazitouni <amirasaidazitouni@gmail.com>
2019-09-17 22:31:59 +0100

    firest cfommit

---
+++ simple/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SimpleComponent } from './Simple.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SimpleComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
